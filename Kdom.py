c5f606511e3187ab4305157bf6e22be3a6211fbb
ashnap07 <66445894+ashnap07@users.noreply.github.com>
2022-10-27 04:22:11 +0300

    added UI

--- Kdom.py
+++ Kdom.py
@@ -1,24 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
 
 
 import numpy as np
 
 
-# In[2]:
 
 
-def pmat(mat):
+
+def pmat(mat,n):
+    val=""
     for iy, ix in np.ndindex(mat.shape):
-        if ix==n-1:
-            print(mat[iy, ix])
+        num=mat[iy, ix]
+        if num==-1:
+            num="K"
         else:
-            print(mat[iy, ix],end="")
+            num=str(num)
+
+        if ix==n-1:
+            val=val+(num+"  \n")
+        else:
+            val=val+(num+"  ")
+    
+    return val
 
 
-# In[3]:
 
 
 def attacked(bd,x,y):
@@ -79,66 +83,58 @@ def nibattacked(bd,x,y):
         return True
 
 
-# In[31]:
 
 
 import itertools as itr
-n=20
-if n<0:
-    print("invalid input")
-    exit()
-k=n*n
-c=0
-bestk=n*n
-bestsol=np.full((n,n),-1)
-while(c<=1000):
-    bd=np.full((n,n),-1)
-    arr1=list(range(n))
-    L=arr1+arr1
-    p=list(itr.permutations(L,2))
+def kights_dom(n):
+
+    if n<0:
+        return "invalid input","invalid input","invalid input"
+    k=n*n
+    bestk=n*n
+    c=0
+    bestsol=np.full((n,n),-1)
+    while(c<1000):
+        bd=np.full((n,n),-1)
+        arr1=list(range(n))
+        L=arr1+arr1
+        p=list(itr.permutations(L,2))
 
 
-    while(len(p)>0): 
-        idx=np.random.randint(len(p))
-        x,y=p[idx][0],p[idx][1]
-        del p[idx]
-        if attacked(bd,x,y):
-            bd[x][y]=0
-        if not nibattacked(bd,x,y):
-            bd[x][y]=-1            
+        while(len(p)>0): 
+            idx=np.random.randint(len(p))
+            x,y=p[idx][0],p[idx][1]
+            del p[idx]
+            if attacked(bd,x,y):
+                bd[x][y]=0
+            if not nibattacked(bd,x,y):
+                bd[x][y]=-1            
 
-    
-    for iy, ix in np.ndindex(bd.shape):     
-        at=attacked(bd,iy,ix)
-        if not at:
-            bd[iy][ix]=-1
-    k=0 
-    for iy, ix in np.ndindex(bd.shape):
-            if bd[iy, ix]==-1:
-                k+=1
-    
-    if k<bestk:
-        bestsol=bd
-        bestk=k
         
-    c+=1
+        for iy, ix in np.ndindex(bd.shape):     
+            at=attacked(bd,iy,ix)
+            if not at:
+                bd[iy][ix]=-1
+        k=0 
+        for iy, ix in np.ndindex(bd.shape):
+                if bd[iy, ix]==-1:
+                    k+=1
+        
+        if k<bestk:
+            bestsol=bd
+            bestk=k
+
+        if c==10:
+            r1=pmat(bestsol,n)+"\nThe Number of knights is "+str(bestk)
+        if c==100:
+            r2=pmat(bestsol,n)+"\nThe Number of knights is "+str(bestk)
+        c+=1
+        r3=pmat(bestsol,n)+"\nThe Number of knights is "+str(bestk)
+
+    return r1,r2,r3
 
 
-# In[32]:
-
-
-print(bestsol)
-print(bestk)
-
-
-# In[ ]:
 
 
 
-
-
-# In[ ]:
-
-
-
-
+#print(str(kights_dom(8)[2]))
---
+++ UI.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import Kdom as kd
+
+header = st.container()
+input = st.container()
+result=st.container()
+
+@st.cache # 👈 This function will be cached
+def Search(s):
+    return kd.kights_dom(s)
+with header:
+    st.title("Knights Domination Generlized Problem ♟")
+
+with input:
+    s=st.text_input("Enter Number of chessboard dimension:")
+    
+    try:
+        s=int(s)
+    except ValueError:
+        s=0
+with result:
+    st.header("Searching result for chessboard of size "+str(s)+"X"+str(s)+" :")
+    r1,r2,r3="","",""
+    try:
+        r1,r2,r3=Search(s)
+    except ValueError:
+        r1,r2,r3=0,0,0
+    st.subheader("Chessboard After 10 itreations:")
+    st.text(r1)
+
+    st.subheader("Chessboard After 100 itreations:")
+    st.text(r2)
+
+    st.subheader("Chessboard After 1000 itreations:")
+    st.text(r3)
+    
